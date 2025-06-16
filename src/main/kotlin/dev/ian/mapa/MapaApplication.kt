d37db1a8e95ba9b7d12cda9d79f7718578f7ddb2
이민성 <dlaepeul@gmail.com>
2025-06-16 16:48:21 +0900

    feat:kotlin gradle default setting

---
+++ src/main/kotlin/dev/ian/mapa/MapaApplication.kt
@@ -0,0 +1,11 @@
+package dev.ian.mapa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MapaApplication
+
+fun main(args: Array<String>) {
+	runApplication<MapaApplication>(*args)
+}
