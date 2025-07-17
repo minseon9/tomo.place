9a9c3365fe45794dae414ac801cf9bee8e1b0382
minseon9 <dlaepeul@gmail.com>
2025-07-18 01:10:54 +0900

    fix:member entity

---
+++ member/src/main/kotlin/dev/ian/mapa/domain/constant/MemberStatus.kt
@@ -0,0 +1,7 @@
+package dev.ian.mapa.domain.constant
+
+enum class MemberStatus {
+    STANDBY,
+    ACTIVATED,
+    DEACTIVATED,
+}
--- member/src/main/kotlin/dev/ian/mapa/domain/entities/MemberEntity.kt
+++ member/src/main/kotlin/dev/ian/mapa/domain/entities/MemberEntity.kt
@@ -1,29 +1,57 @@
 package dev.ian.mapa.domain.entities
 
+import dev.ian.mapa.domain.constant.MemberStatus
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import java.time.LocalDateTime
 
+/*
+도메인 모델 (Entity)
+- 기본적인 정책을 가지고 수행. 해당 모델로서 검증할 수 있는 영역에 대한 검증 수행
+ */
 @Entity
+@Table(name = "member")
 class MemberEntity(
-    val email: String,
-    var password: String,
-    var name: String,
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long? = null,
+    val id: Long = 0,
+    @Column(unique = true, nullable = false)
+    val email: String,
+    @Column(nullable = false)
+    var password: String,
+    @Column(nullable = false)
+    var username: String,
+    @Column()
+    var nickname: String? = null,
+    @Enumerated(EnumType.STRING)
+    var status: MemberStatus = MemberStatus.STANDBY,
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    val updatedAt: LocalDateTime = LocalDateTime.now(),
+    @Column(name = "deleted_at")
+    var deletedAt: LocalDateTime? = null,
 ) {
     companion object {
         fun create(
             email: String,
             password: String,
-            name: String,
+            username: String,
         ): MemberEntity {
             require(email.isNotBlank()) { "이메일은 필수입니다." }
             require(password.length >= 8) { "비밀번호는 8자 이상이어야 합니다." }
-            require(name.isNotBlank()) { "이름은 필수입니다." }
+            require(username.isNotBlank()) { "이름은 필수입니다." }
 
-            return MemberEntity(email, password, name)
+            return MemberEntity(email = email, password = password, username = username)
         }
     }
 }
