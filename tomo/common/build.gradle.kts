f3d05fe4b91d2370e29365aa7fa7d167f66e76ff
minseon9 <dlaepeul@gmail.com>
2025-07-24 19:55:43 +0900

    feat:common HttpClient

---
+++ tomo/common/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework:spring-context")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+} 
