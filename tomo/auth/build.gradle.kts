903c84ae95da52e4a36bdfa5877c2eb7760818c5
minseon9 <dlaepeul@gmail.com>
2025-08-09 00:06:07 +0900

    refactor:delete un-used subprojects

--- tomo/auth/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-dependencies {
-    implementation(project(":member"))
-    implementation(project(":contract"))
-
-    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
-}
