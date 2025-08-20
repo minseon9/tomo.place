07c94cacd9e691e1a64bd6b7235be320a92aa6f6
minseon9 <dlaepeul@gmail.com>
2025-08-20 10:58:24 +0900

    fix:feignclient 제거

--- tomo/tomo/src/main/kotlin/place/tomo/TomoApplication.kt
+++ tomo/tomo/src/main/kotlin/place/tomo/TomoApplication.kt
@@ -4,9 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
-// import org.springframework.cloud.openfeign.EnableFeignClients
-
-// @EnableFeignClients
 @SpringBootApplication
 @ConfigurationPropertiesScan
 class TomoApplication
