912c34a5af1e45868edbbadb7cd74408e057afb4
minseon9 <dlaepeul@gmail.com>
2025-08-21 23:01:43 +0900

    wip

--- tomo/build.gradle.kts
+++ tomo/build.gradle.kts
@@ -1,5 +1,3 @@
-// buildSrc import 제거 - Liquibase 설정은 기존 로직 유지
-
 plugins {
     alias(libs.plugins.spring.boot) apply false
     alias(libs.plugins.spring.dependency.management) apply false
@@ -46,16 +44,13 @@ subprojects {
         enabled = true
     }
 
-    // buildSrc로 분리된 테스트 설정 적용
     TestConfig.configureTestTasks(this)
 
     dependencies {
-        // buildSrc로 분리된 의존성 그룹 적용
         productionDependencies()
         testDependencies()
     }
 
-    // Liquibase 활성화 여부 (모듈별 opt-in)
     val liquibaseEnabled: Boolean = (findProperty("liquibaseEnabled") as String?)?.toBoolean() == true
 
     if (liquibaseEnabled) {
@@ -66,7 +61,90 @@ subprojects {
             add("liquibaseRuntime", sourceSets.main.get().runtimeClasspath)
         }
 
-        // buildSrc로 분리된 Liquibase 설정 적용
+//        plugins.withId("org.liquibase.gradle") {
+//            val mainProjectName = rootProject.findProperty("mainProjectName")
+//            val isMainProject = project.name == mainProjectName
+//            val moduleBasePath = "${project.name}/src/main/resources/db/changelog"
+//            val moduleMainChangelog = "$moduleBasePath/db.changelog-${project.name}.yml"
+//            val mainAggregateChangelog = "$mainProjectName/src/main/resources/db/changelog/db.changelog-main.yml"
+//            val mainProps = rootProject.file("$mainProjectName/src/main/resources/application.properties")
+//
+//            configure<org.liquibase.gradle.LiquibaseExtension> {
+//                val cfg = DbPropsLoader.load(mainProps)
+//
+//                val changeLogFilePath = if (isMainProject) mainAggregateChangelog else moduleMainChangelog
+//
+//                // searchPath: 루트 + (메인프로젝트인 경우 모든 활성 모듈의 changelog 디렉토리, 그 외는 해당 모듈)
+//                val searchPaths: String =
+//                    buildList {
+//                        add(project.relativePath(rootProject.projectDir))
+//                        if (isMainProject) {
+//                            val enabledModules =
+//                                rootProject.subprojects.filter {
+//                                    (it.findProperty("liquibaseEnabled") as String?)?.toBoolean() == true
+//                                }
+//                            addAll(
+//                                enabledModules.map {
+//                                    project.relativePath(it.projectDir.resolve("src/main/resources/db/changelog"))
+//                                },
+//                            )
+//                        } else {
+//                            add(project.relativePath(project.projectDir.resolve("src/main/resources/db/changelog")))
+//                        }
+//                    }.joinToString(",")
+//
+//                activities.register("main") {
+//                    arguments =
+//                        mapOf(
+//                            "changeLogFile" to changeLogFilePath,
+//                            "url" to cfg.url,
+//                            "username" to cfg.username,
+//                            "password" to cfg.password,
+//                            "driver" to cfg.driver,
+//                            "logLevel" to "DEBUG",
+//                            "verbose" to "true",
+//                            "searchPath" to searchPaths,
+//                        )
+//                }
+//            }
+//
+//            // 메인 changelog에 현재 모듈의 changelog include 추가 (init 이후)
+//            val initMigration = tasks.register<InitMigrationTask>("initMigration")
+//            if (!isMainProject) {
+//                val appendMain =
+//                    tasks.register<AppendIncludeTask>("appendMainInclude") {
+//                        // Include module changelog in aggregate changelog
+//                        targetChangelogPath = rootProject.file(mainAggregateChangelog).absolutePath
+//                        includeFilePath = moduleMainChangelog
+//                        mustRunAfter(initMigration)
+//                    }
+//                initMigration.configure { finalizedBy(appendMain) }
+//            }
+//
+//            val ts = System.currentTimeMillis()
+//            val desc = (project.findProperty("desc") as String?) ?: "change"
+//            val outFile = File(project.projectDir, "src/main/resources/db/changelog/migrations/$ts-$desc-changelog-${project.name}.yml")
+//            val entityPkg =
+//                (project.findProperty("liquibaseEntityPackage") as String?)
+//                    ?: "place.tomo.${project.name}.domain.entities"
+//
+//            val generateMigration =
+//                tasks.register<GenerateMigrationTask>("generateMigration") {
+//                    liquibaseClasspath = configurations.getByName("liquibaseRuntime")
+//                    entityPackage = entityPkg
+//                    propertiesFile = mainProps
+//                    changelogOutputFile = outFile
+//                }
+//            generateMigration.configure { dependsOn(tasks.named("classes"), initMigration) }
+//            val appendModule =
+//                tasks.register<AppendIncludeTask>("appendMigrationInclude") {
+//                    // Include generated migration file in module changelog
+//                    targetChangelogPath = file("src/main/resources/db/changelog/db.changelog-${project.name}.yml").absolutePath
+//                    includeFilePath = "migrations/$ts-$desc-changelog-${project.name}.yml"
+//                    mustRunAfter(generateMigration)
+//                }
+//            generateMigration.configure { finalizedBy(appendModule) }
+//        }
         LiquibaseConfig.configureLiquibase(this)
     }
 }
--- tomo/buildSrc/src/main/kotlin/LiquibaseConfig.kt
+++ tomo/buildSrc/src/main/kotlin/LiquibaseConfig.kt
@@ -1,154 +1,111 @@
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.register
+import org.liquibase.gradle.LiquibaseExtension
+import java.io.File
 
-/**
- * Liquibase 설정을 중앙에서 관리
- */
 object LiquibaseConfig {
-    
     fun configureLiquibase(project: Project) {
         project.plugins.withId("org.liquibase.gradle") {
             configureLiquibaseTasks(project)
         }
     }
-    
+
     private fun configureLiquibaseTasks(project: Project) {
-        // 커스텀 Liquibase 태스크들
+        registerMainActivity(project)
         createInitMigrationTask(project)
         createAppendIncludeTask(project)
         createGenerateMigrationTask(project)
     }
-    
+
+    private fun registerMainActivity(project: Project) {
+        val mainProjectName = project.rootProject.findProperty("mainProjectName") as String
+        val isMainProject = project.name == mainProjectName
+        val moduleBasePath = "${project.name}/src/main/resources/db/changelog"
+        val moduleMainChangelog = "$moduleBasePath/db.changelog-${project.name}.yml"
+        val mainAggregateChangelog = "$mainProjectName/src/main/resources/db/changelog/db.changelog-main.yml"
+        val mainProps = project.rootProject.file("$mainProjectName/src/main/resources/application.properties")
+
+        val cfg = DbPropsLoader.load(mainProps)
+        val changeLogFilePath = if (isMainProject) mainAggregateChangelog else moduleMainChangelog
+
+        val searchPaths: String =
+            buildList {
+                add(project.relativePath(project.rootProject.projectDir))
+                if (isMainProject) {
+                    val enabledModules =
+                        project.rootProject.subprojects.filter {
+                            (it.findProperty("liquibaseEnabled") as String?)?.toBoolean() == true
+                        }
+                    addAll(
+                        enabledModules.map {
+                            project.relativePath(it.projectDir.resolve("src/main/resources/db/changelog"))
+                        },
+                    )
+                } else {
+                    add(project.relativePath(project.projectDir.resolve("src/main/resources/db/changelog")))
+                }
+            }.joinToString(",")
+
+        project.extensions.configure<LiquibaseExtension> {
+            activities.register("main") {
+                arguments =
+                    mapOf(
+                        "changeLogFile" to changeLogFilePath,
+                        "url" to cfg.url,
+                        "username" to cfg.username,
+                        "password" to cfg.password,
+                        "driver" to cfg.driver,
+                        "logLevel" to "DEBUG",
+                        "verbose" to "true",
+                        "searchPath" to searchPaths,
+                    )
+            }
+        }
+    }
+
     private fun createInitMigrationTask(project: Project) {
         project.tasks.register<InitMigrationTask>("initMigration") {
             group = "liquibase"
             description = "Initialize Liquibase migration"
-            
-            targetChangelogPath.set("src/main/resources/db/changelog/db.changelog-main.yml")
-            includeFilePath.set("src/main/resources/db/changelog/migrations")
-            
+
+//            targetChangelogPath.set("src/main/resources/db/changelog/db.changelog-main.yml")
+//            includeFilePath.set("src/main/resources/db/changelog/migrations")
+
             dependsOn("compileKotlin")
         }
     }
-    
-    private fun createAppendIncludeTask(project: Project) {
-        project.tasks.register<AppendIncludeTask>("appendInclude") {
-            group = "liquibase"
-            description = "Append include statement to changelog"
-            
-            targetChangelogPath.set("src/main/resources/db/changelog/db.changelog-main.yml")
-            includeFilePath.set("src/main/resources/db/changelog/migrations")
-            
-            dependsOn("compileKotlin")
-        }
-    }
-    
+
     private fun createGenerateMigrationTask(project: Project) {
+        val ts = System.currentTimeMillis()
+        val desc = (project.findProperty("desc") as String?) ?: "change"
+        val outFile = File(project.projectDir, "src/main/resources/db/changelog/migrations/$ts-$desc-changelog-${project.name}.yml")
+        val entityPkg =
+            (project.findProperty("liquibaseEntityPackage") as String?)
+                ?: "place.tomo.${project.name}.domain.entities"
+
         project.tasks.register<GenerateMigrationTask>("generateMigration") {
             group = "liquibase"
             description = "Generate Liquibase migration from Hibernate entities"
-            
-            entityPackage.set("place.tomo")
-            propertiesFile.set("src/main/resources/application.properties")
-            changelogOutputFile.set("src/main/resources/db/changelog/migrations")
-            
+            liquibaseClasspath = configurations.getByName("liquibaseRuntime")
+            entityPackage = entityPkg
+            propertiesFile = mainProps
+            changelogOutputFile = outFile
+
             dependsOn("compileKotlin")
             finalizedBy("appendInclude")
         }
     }
-}
 
-/**
- * Liquibase 마이그레이션 초기화 태스크
- */
-abstract class InitMigrationTask : org.gradle.api.DefaultTask() {
-    
-    abstract val targetChangelogPath: org.gradle.api.provider.Property<String>
-    abstract val includeFilePath: org.gradle.api.provider.Property<String>
-    
-    @org.gradle.api.tasks.TaskAction
-    fun execute() {
-        val targetFile = project.file(targetChangelogPath.get())
-        val includeDir = project.file(includeFilePath.get())
-        
-        if (!targetFile.exists()) {
-            targetFile.parentFile.mkdirs()
-            targetFile.writeText("""
-                databaseChangeLog:
-                  - include:
-                      file: db/changelog/migrations/
-            """.trimIndent())
-            logger.lifecycle("Created changelog file: ${targetFile.absolutePath}")
-        }
-        
-        if (!includeDir.exists()) {
-            includeDir.mkdirs()
-            logger.lifecycle("Created migrations directory: ${includeDir.absolutePath}")
-        }
-    }
-}
+    private fun createAppendIncludeTask(project: Project) {
+        project.tasks.register<AppendIncludeTask>("appendInclude") {
+            group = "liquibase"
+            description = "Append include statement to changelog"
 
-/**
- * Liquibase include 문 추가 태스크
- */
-abstract class AppendIncludeTask : org.gradle.api.DefaultTask() {
-    
-    abstract val targetChangelogPath: org.gradle.api.provider.Property<String>
-    abstract val includeFilePath: org.gradle.api.provider.Property<String>
-    
-    @org.gradle.api.tasks.TaskAction
-    fun execute() {
-        val targetFile = project.file(targetChangelogPath.get())
-        val includeDir = project.file(includeFilePath.get())
-        
-        if (!targetFile.exists()) {
-            logger.warn("Target changelog file does not exist: ${targetFile.absolutePath}")
-            return
-        }
-        
-        val existingContent = targetFile.readText()
-        val newMigrations = includeDir.listFiles { file -> 
-            file.isFile && file.extension == "yml" && file.name.startsWith("V")
-        }?.sortedBy { it.name } ?: emptyList()
-        
-        val newIncludes = newMigrations.map { migration ->
-            "  - include:\n      file: db/changelog/migrations/${migration.name}"
-        }.joinToString("\n")
-        
-        if (newIncludes.isNotEmpty()) {
-            val updatedContent = existingContent + "\n" + newIncludes
-            targetFile.writeText(updatedContent)
-            logger.lifecycle("Updated changelog with new migrations")
-        }
-    }
-}
+            targetChangelogPath = File("src/main/resources/db/changelog/db.changelog-${project.name}.yml").absolutePath
+            includeFilePath = "migrations/$ts-$desc-changelog-${project.name}.yml"
 
-/**
- * Liquibase 마이그레이션 생성 태스크
- */
-abstract class GenerateMigrationTask : org.gradle.api.DefaultTask() {
-    
-    abstract val entityPackage: org.gradle.api.provider.Property<String>
-    abstract val propertiesFile: org.gradle.api.provider.Property<String>
-    abstract val changelogOutputFile: org.gradle.api.provider.Property<String>
-    
-    @org.gradle.api.tasks.TaskAction
-    fun execute() {
-        val outputDir = project.file(changelogOutputFile.get())
-        outputDir.mkdirs()
-        
-        val timestamp = System.currentTimeMillis()
-        val migrationFile = outputDir.resolve("V${timestamp}__auto_generated_migration.yml")
-        
-        migrationFile.writeText("""
-            databaseChangeLog:
-              - changeSet:
-                  id: ${timestamp}
-                  author: auto-generated
-                  changes:
-                    # Auto-generated migration - please review and modify as needed
-        """.trimIndent())
-        
-        logger.lifecycle("Generated migration file: ${migrationFile.absolutePath}")
+            dependsOn("compileKotlin")
+        }
     }
 }
