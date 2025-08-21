6f14b7027ab9f39dff3cd1d3c1623921948efb3c
minseon9 <dlaepeul@gmail.com>
2025-08-21 23:01:12 +0900

    style:remove comment and execute lint

--- tomo/buildSrc/build.gradle.kts
+++ tomo/buildSrc/build.gradle.kts
@@ -7,6 +7,5 @@ repositories {
 }
 
 dependencies {
-    // kotlin-dsl 플러그인이 Gradle API/Tooling API 제공
     implementation("org.yaml:snakeyaml:2.2")
 }
--- tomo/buildSrc/src/main/kotlin/TestConfig.kt
+++ tomo/buildSrc/src/main/kotlin/TestConfig.kt
@@ -3,15 +3,11 @@ import org.gradle.api.tasks.testing.Test
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.kotlin.dsl.withType
 
-/**
- * 테스트 설정을 중앙에서 관리
- */
 object TestConfig {
-    
     fun configureTestTasks(project: Project) {
         project.tasks.withType<Test> {
             useJUnitPlatform()
-            
+
             testLogging {
                 events("started", "passed", "skipped", "failed", "standardOut", "standardError")
                 showStandardStreams = true
