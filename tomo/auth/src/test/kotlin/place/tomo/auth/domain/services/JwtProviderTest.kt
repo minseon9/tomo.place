be232e172b75678c7660a9bdf26976c3f23509de
minseon9 <dlaepeul@gmail.com>
2025-08-27 22:15:24 +0900

    fix:jwt provider에서 encode하는 대상에 대한 검증 추가

--- tomo/auth/src/test/kotlin/place/tomo/auth/domain/services/JwtProviderTest.kt
+++ tomo/auth/src/test/kotlin/place/tomo/auth/domain/services/JwtProviderTest.kt
@@ -1,18 +1,29 @@
 package place.tomo.auth.domain.services
 
+import io.mockk.every
 import io.mockk.mockk
+import io.mockk.slot
 import io.mockk.verify
 import net.datafaker.Faker
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm
+import org.springframework.security.oauth2.jwt.JwsHeader
+import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.security.oauth2.jwt.JwtClaimsSet
 import org.springframework.security.oauth2.jwt.JwtEncoder
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters
 import place.tomo.auth.domain.dtos.JwtPropertiesDTO
+import java.time.Instant
 
-@DisplayName("JwtTokenProvider")
+@DisplayName("JwtProvider")
 class JwtProviderTest {
     private val faker: Faker = Faker()
+
     private val jwtEncoder: JwtEncoder = mockk(relaxed = true)
+
     private val jwtProperties: JwtPropertiesDTO =
         JwtPropertiesDTO(
             issuer = faker.name().fullName(),
@@ -32,17 +43,56 @@ class JwtProviderTest {
         @Test
         @DisplayName("주어진 사용자 이메일로 서명된 JWT Access 토큰을 발급한다")
         fun `issue access token when subject provided expect signed jwt generated`() {
-            val email = faker.internet().emailAddress()
-            provider.issueAccessToken(email)
+            val subject = faker.internet().emailAddress()
 
-            verify { jwtEncoder.encode(any()) }
+            val mockJwt =
+                mockk<Jwt> {
+                    every { tokenValue } returns "test-access-token"
+                }
+            every { jwtEncoder.encode(any()) } returns mockJwt
+
+            val issueToken = provider.issueAccessToken(subject)
+
+            assertThat(issueToken).isEqualTo("test-access-token")
+
+            val parametersSlot = slot<JwtEncoderParameters>()
+            verify { jwtEncoder.encode(capture(parametersSlot)) }
+
+            val capturedParameters = parametersSlot.captured
+            val claims = capturedParameters.claims
+
+            assertThat(claims.getClaim<String>("iss")).isEqualTo(jwtProperties.issuer)
+            assertThat(claims.getClaim<String>("sub")).isEqualTo(subject)
+            assertThat(claims.getClaim<List<String>>("aud")).isEqualTo(jwtProperties.audiences)
+            assertThat(claims.getClaim<Instant>("iat")).isBefore(Instant.now())
+            assertThat(claims.getClaim<Instant>("exp")).isAfter(Instant.now())
         }
 
         @Test
         @DisplayName("주어진 사용자 이메일로 서명된 JWT Refresh 토큰을 발급한다")
         fun `issue refresh token when subject provided expect signed jwt generated`() {
-            val email = faker.internet().emailAddress()
-            provider.issueRefreshToken(email)
+            val subject = faker.internet().emailAddress()
+            val mockJwt =
+                mockk<Jwt> {
+                    every { tokenValue } returns "test-refresh-token"
+                }
+            every { jwtEncoder.encode(any()) } returns mockJwt
+
+            val refreshToken = provider.issueRefreshToken(subject)
+
+            assertThat(refreshToken).isEqualTo("test-refresh-token")
+
+            val parametersSlot = slot<JwtEncoderParameters>()
+            verify { jwtEncoder.encode(capture(parametersSlot)) }
+
+            val capturedParameters = parametersSlot.captured
+            val claims = capturedParameters.claims
+
+            assertThat(claims.getClaim<String>("iss")).isEqualTo(jwtProperties.issuer)
+            assertThat(claims.getClaim<String>("sub")).isEqualTo(subject)
+            assertThat(claims.getClaim<List<String>>("aud")).isEqualTo(jwtProperties.audiences)
+            assertThat(claims.getClaim<Instant>("iat")).isBefore(Instant.now())
+            assertThat(claims.getClaim<Instant>("exp")).isAfter(Instant.now())
 
             verify { jwtEncoder.encode(any()) }
         }
