f1177fb1fb9335760dc7c1740a9907a0c35adc8f
minseon9 <dlaepeul@gmail.com>
2025-08-28 21:35:10 +0900

    fix:login endpoint 제거

--- tomo/auth/src/main/kotlin/place/tomo/auth/application/requests/AuthenticateRequest.kt
+++ tomo/auth/src/main/kotlin/place/tomo/auth/application/requests/AuthenticateRequest.kt
@@ -1,13 +1,6 @@
 package place.tomo.auth.application.requests
 
-import place.tomo.contract.constant.OIDCProviderType
-
 data class EmailPasswordAuthenticateRequest(
     val email: String,
     val password: String,
-)
-
-data class OIDCAuthenticateRequest(
-    val provider: OIDCProviderType,
-    val authorizationCode: String,
 )
--- tomo/auth/src/main/kotlin/place/tomo/auth/ui/controllers/AuthController.kt
+++ tomo/auth/src/main/kotlin/place/tomo/auth/ui/controllers/AuthController.kt
@@ -6,10 +6,8 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import place.tomo.auth.application.requests.OIDCAuthenticateRequest
 import place.tomo.auth.application.requests.OIDCSignUpRequest
 import place.tomo.auth.application.services.OIDCApplicationService
-import place.tomo.auth.ui.requests.LoginRequestBody
 import place.tomo.auth.ui.requests.SignupRequestBody
 import place.tomo.auth.ui.responses.LoginResponseBody
 
@@ -23,15 +21,6 @@ class AuthController(
         @RequestBody @Valid body: SignupRequestBody,
     ): ResponseEntity<LoginResponseBody> {
         val response = oidcAuthService.signUp(OIDCSignUpRequest(body.provider, body.authorizationCode))
-
-        return ResponseEntity.ok(LoginResponseBody(token = response.token, refreshToken = response.refreshToken))
-    }
-
-    @PostMapping("/login")
-    fun authenticate(
-        @RequestBody @Valid body: LoginRequestBody,
-    ): ResponseEntity<LoginResponseBody> {
-        val response = oidcAuthService.authenticate(OIDCAuthenticateRequest(body.provider, body.authorizationCode))
 
         return ResponseEntity.ok(LoginResponseBody(token = response.token, refreshToken = response.refreshToken))
     }
