21e957760fb4be7a1f5e0ca4398d63550897ba19
minseon9 <dlaepeul@gmail.com>
2025-08-28 16:03:05 +0900

    feat:spring api docs

--- tomo/tomo/src/main/kotlin/place/tomo/infra/config/security/SecurityFilterChainConfig.kt
+++ tomo/tomo/src/main/kotlin/place/tomo/infra/config/security/SecurityFilterChainConfig.kt
@@ -51,6 +51,8 @@ class SecurityFilterChainConfig {
                 auth
                     .requestMatchers(HttpMethod.POST, *publicPostEndpoints)
                     .permitAll()
+                    .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**")
+                    .permitAll()
                     .anyRequest()
                     .authenticated()
             }.oauth2ResourceServer { auth ->
