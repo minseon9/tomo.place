b53b762b114f1978d402934b2ba5536957f28b59
minseon9 <dlaepeul@gmail.com>
2025-09-03 00:38:52 +0900

    refactor:test 용 security filter에서는 jwt auth를 하지 않도록 수정

--- tomo/common/src/main/kotlin/place/tomo/common/test/security/TestSecurityConfig.kt
+++ tomo/common/src/main/kotlin/place/tomo/common/test/security/TestSecurityConfig.kt
@@ -2,23 +2,17 @@ package place.tomo.common.test.security
 
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Primary
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @TestConfiguration
-@Import(TestJwtFilter::class)
-class TestSecurityConfig(
-    private val testJwtFilter: TestJwtFilter,
-) {
+class TestSecurityConfig {
     private val publicPostEndpoints =
         arrayOf(
             "/api/auth/signup",
-            "/api/oidc/signup",
         )
 
     @Bean
@@ -26,7 +20,7 @@ class TestSecurityConfig(
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain =
         http
             .csrf { csrf ->
-                csrf.ignoringRequestMatchers(*publicPostEndpoints)
+                csrf.disable()
             }.authorizeHttpRequests { auth ->
                 auth
                     .requestMatchers(HttpMethod.POST, *publicPostEndpoints)
@@ -36,8 +30,7 @@ class TestSecurityConfig(
             }.formLogin { it.disable() }
             .sessionManagement {
                 it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            }.addFilterBefore(testJwtFilter, UsernamePasswordAuthenticationFilter::class.java)
-            .logout { logout ->
+            }.logout { logout ->
                 logout.permitAll()
             }.httpBasic { it.disable() }
             .build()
