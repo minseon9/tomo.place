13734fa845d7a00ee6f935a1b5c0234e3fcdfc42
minseon9 <dlaepeul@gmail.com>
2025-09-03 20:28:06 +0900

    refactor:사용안하는 클래스들 제거

--- app/lib/domains/auth/consts/routes.dart
+++
@@ -1,6 +0,0 @@
-/// Auth 도메인 라우트 상수
-class AuthRoutes {
-  static const String signup = '/signup';
-  static const String privacyPolicy = '/privacy-policy';
-  static const String termsOfService = '/terms-of-service';
-}
--- app/lib/domains/auth/core/entities/social_provider.dart
+++
@@ -1,9 +0,0 @@
-// 도메인 상수: 소셜 로그인 제공자 종류
-enum SocialProvider {
-  kakao('KAKAO'),
-  apple('APPLE'), 
-  google('GOOGLE');
-
-  const SocialProvider(this.code);
-  final String code;
-}
