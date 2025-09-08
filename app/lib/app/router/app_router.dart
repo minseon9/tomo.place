9ba46edc25fc89d8f236a9eb68ad97265932a84c
minseon9 <dlaepeul@gmail.com>
2025-09-09 00:10:23 +0900

    feat:term routes app에 등록

--- app/lib/app/router/app_router.dart
+++ app/lib/app/router/app_router.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../domains/auth/presentation/routes/auth_routes.dart';
+import '../../domains/terms_agreement/presentation/routes/terms_routes.dart';
 import '../pages/splash_page.dart';
 
 Route<dynamic> _generateRoute(RouteSettings settings) {
   final Map<String, WidgetBuilder> builders = {
     ...AuthRoutes.builders,
+    ...TermsRoutes.builders,
 
     '/splash': (context) => const SplashPage(),
-    
+
     '/home': (context) =>
         const Scaffold(body: Center(child: Text('홈 화면 (추후 구현)'))),
   };
