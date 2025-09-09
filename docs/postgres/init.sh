3273b2e44bf465bbb55836b005e64bb70461bc30
minseon9 <dlaepeul@gmail.com>
2025-09-10 02:18:16 +0900

    chore:db init shell script logging

--- docs/postgres/init.sh
+++ docs/postgres/init.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
+echo "[INFO] Creating database 'tomo' ..."
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE tomo;
 EOSQL
 
+echo "[INFO] Installing PostgreSQL extensions ..."
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=tomo <<-EOSQL
     CREATE EXTENSION IF NOT EXISTS postgis;
     CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
@@ -12,3 +14,4 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname=tomo <<-EOSQL
     CREATE EXTENSION IF NOT EXISTS unaccent;
 EOSQL
 
+echo "[INFO] Database initialization completed ..."
