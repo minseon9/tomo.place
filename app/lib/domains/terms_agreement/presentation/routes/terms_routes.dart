a13276ac359b6de38447d2a8ad8eef46cd07fa83
minseon9 <dlaepeul@gmail.com>
2025-09-10 06:41:24 +0900

    fix:의미없는 Init 제거

--- app/lib/domains/terms_agreement/presentation/routes/terms_routes.dart
+++ app/lib/domains/terms_agreement/presentation/routes/terms_routes.dart
@@ -6,8 +6,6 @@ import '../pages/privacy_policy_page.dart';
 import '../pages/terms_of_service_page.dart';
 
 class TermsRoutes {
-  TermsRoutes._();
-
   static Map<String, WidgetBuilder> get builders => {
     Routes.termsOfService: (context) => const TermsOfServicePage(),
     Routes.privacyPolicy: (context) => const PrivacyPolicyPage(),
