de10f2b6fdb96016920074641c7297309eec503d
minseon9 <dlaepeul@gmail.com>
2025-09-19 17:25:51 +0900

    refactor:provider -> service로 네이밍 변경

--- app/lib/domains/auth/data/oauth/oauth_service.dart
+++ app/lib/domains/auth/data/oauth/oauth_service.dart
@@ -1,11 +1,13 @@
 import 'oauth_result.dart';
 
-abstract class OAuthProvider {
+abstract class OAuthService<T> {
   String get providerId;
-
+  
   String get displayName;
 
   bool get isSupported;
+  
+  T get config;
 
   Future<OAuthResult> signIn();
 
