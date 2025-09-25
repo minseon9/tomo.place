665cfbfd28e9ba5a73b9dbfbaa3fee1a2ffcb69b
minseon9 <dlaepeul@gmail.com>
2025-09-26 01:25:52 +0900

    refactor:exception notifier에서 report된 에러 가공하도록 수정

--- app/lib/shared/exception_handler/exception_notifier.dart
+++ app/lib/shared/exception_handler/exception_notifier.dart
@@ -1,11 +1,18 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'exceptions/unknown_exception.dart';
 import 'models/exception_interface.dart';
 
 class ExceptionNotifier extends StateNotifier<ExceptionInterface?> {
   ExceptionNotifier() : super(null);
 
-  void report(ExceptionInterface error) => state = error;
+  void report(error) {
+    if (error is ExceptionInterface) {
+      state = error;
+    } else {
+      state = UnknownException(message: error.toString());
+    }
+  }
 
   void clear() => state = null;
 }
